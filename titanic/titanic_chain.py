760b063660630c5f5f599724363eb3983c79e662
kenta-s <knt01222@gmail.com>
2017-07-05 09:38:54 +0900

    Modify train.py

---
+++ titanic/titanic_chain.py
@@ -0,0 +1,25 @@
+import numpy as np
+# import matplotlib.pyplot as plt
+# import chainer
+# from chainer import cuda, Function, gradient_check, Variable, optimizers, serializers, utils
+from chainer import Link, Chain, ChainList
+import chainer.functions as F
+import chainer.links as L
+
+class TitanicChain(Chain):
+    def __init__(self, input_length):
+        super(TitanicChain, self).__init__(
+            l1 = L.Linear(input_length, 5),
+            l2 = L.Linear(5, 3),
+            l3 = L.Linear(3, 1)
+        )
+
+    def __call__(self, x, y):
+        # return F.mean_squared_error(self.fwd(x), y)
+        return F.binary_accuracy(self.fwd(x), y)
+
+    def fwd(self, x):
+        h1 = F.softmax(self.l1(x))
+        h2 = self.l2(h1)
+        h3 = self.l3(h2)
+        return h3
--- titanic/train.py
+++ titanic/train.py
@@ -1,6 +1,13 @@
 from variable_builder import VariableBuilder
 from chainer import Variable, optimizers, serializers
 from IPython import embed
+from titanic_chain import TitanicChain
 
 variable_builder = VariableBuilder()
-train_data = Variable(variable_builder())
+x_train_data = Variable(variable_builder())
+# y_train_data
+row, col = train_data.shape
+
+titanic_chain = TitanicChain(col)
+
+embed()
