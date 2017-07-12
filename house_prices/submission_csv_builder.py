85ed88bffa95c14241d9534f7a391b063d1ef4eb
kenta-s <knt01222@gmail.com>
2017-07-12 09:20:11 +0900

    Complete house price

--- house_prices/submission_csv_builder.py
+++ house_prices/submission_csv_builder.py
@@ -5,6 +5,7 @@ from chainer.training import extensions
 import numpy as np
 from IPython import embed
 from house_price_chain import HousePriceChain, Classifier
+import pandas as pd
 
 data_builder = DataBuilder('test.csv')
 data_builder.build_doc()
@@ -18,4 +19,11 @@ optimizer.setup(model)
 
 serializers.load_npz('house_price.npz', model)
 
-embed()
+def predict(target):
+    return int(model.predictor(np.array([target])).data)
+
+house_price_ids = list(pd.read_csv('test.csv').Id)
+house_price_list = list(map(predict, test))
+answer = {'Id': house_price_ids, 'SalePrice': house_price_list}
+df = pd.DataFrame(answer)
+df.to_csv('answer.csv', index=None)
