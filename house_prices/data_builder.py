71ae3829e8dc10955687b9278cbe525804f73208
kenta-s <knt01222@gmail.com>
2017-07-14 09:07:42 +0900

    Implement calc_avg

--- house_prices/data_builder.py
+++ house_prices/data_builder.py
@@ -22,6 +22,7 @@ class DataBuilder():
         self.doc.save_as_text('house_price.dict')
 
     def build_np_array(self):
+        embed()
         for row in self.df.drop("SalePrice", axis=1).values.tolist():
             array = list(map(self.convert_to_float, row))
             self.train_data.append(np.array(array).astype(np.float32))
@@ -34,6 +35,21 @@ class DataBuilder():
             train.append((self.train_data[i], np.array([self.df.SalePrice[i]], dtype=np.float32)))
 
         return train
+
+    def calc_avg(self, col):
+        col_dict = {}
+        target_col_list = list(set(self.df[col]))
+        for c in target_col_list:
+            col_dict[str(c)] = {"count":0, "total":0}
+        for row in self.df[[col, "SalePrice"]].values.tolist():
+            col_dict[str(row[0])]["count"] += 1
+            col_dict[str(row[0])]["total"] += row[1]
+
+        for c in target_col_list :
+            col_dict[str(c)]["avg"] = col_dict[str(c)]["total"] / col_dict[str(c)]["count"]
+            
+        return col_dict
+       
 
     def build_np_array_for_submission(self):
         for row in self.df.values.tolist():
