4bcc60f6a75bccfb58c93e5bc93945ef956a1706
kenta-s <knt01222@gmail.com>
2018-10-15 09:14:27 +0900

    WIP

--- house_prices/house_prices.py
+++ house_prices/house_prices.py
@@ -2,19 +2,50 @@ from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 import pandas as pd
 import sklearn.model_selection as sk
+from gensim import corpora
 
 from IPython import embed
 
+def create_dic(df, target_list):
+    dic = {}
+    for target in target_list:
+        dic[target] = corpora.Dictionary([df[target].unique()])
+    return dic
+
 df = pd.read_csv("train.csv")
+
+# TODO: remove invalid columns
+TARGET_LIST = [
+    'MSZoning',
+    'Street',
+    'Alley',
+    'LotShape',
+    'LandContour',
+    'Utilities',
+    'LotConfig',
+    'LandSlope',
+    'Neighborhood',
+    'Condition1',
+    'Condition2',
+    'BldgType',
+    'HouseStyle',
+    'RoofStyle',
+    'RoofMatl',
+    'Exterior1st','Exterior2nd','MasVnrType','ExterQual','ExterCond','Foundation','BsmtQual','BsmtCond','BsmtExposure','BsmtFinType1','BsmtFinType2','Heating','HeatingQC','CentralAir','Electrical','1stFlrSF','2ndFlrSF','LowQualFinSF','GrLivArea','BsmtFullBath','BsmtHalfBath','FullBath','HalfBath','BedroomAbvGr','KitchenAbvGr','KitchenQual','TotRmsAbvGrd','Functional','Fireplaces','FireplaceQu','GarageType','GarageYrBlt','GarageFinish','GarageCars','GarageArea','GarageQual','GarageCond','PavedDrive','WoodDeckSF','OpenPorchSF','EnclosedPorch','3SsnPorch','ScreenPorch','PoolArea','PoolQC','Fence','MiscFeature','MiscVal','MoSold','YrSold','SaleType','SaleCondition'
+]
+
+# dic = create_dic(df, target_list)
+
+# mszoning_dic = corpora.Dictionary([df.MSZoning.unique()])
+# street_dic = corpora.Dictionary([df.Street.unique()])
+# alley_dic = corpora.Dictionary([df.Alley.unique()])
 X = df[['MSSubClass','MSZoning','LotFrontage','LotArea','Street','Alley','LotShape','LandContour','Utilities','LotConfig','LandSlope','Neighborhood','Condition1','Condition2','BldgType','HouseStyle','OverallQual','OverallCond','YearBuilt','YearRemodAdd','RoofStyle','RoofMatl','Exterior1st','Exterior2nd','MasVnrType','MasVnrArea','ExterQual','ExterCond','Foundation','BsmtQual','BsmtCond','BsmtExposure','BsmtFinType1','BsmtFinSF1','BsmtFinType2','BsmtFinSF2','BsmtUnfSF','TotalBsmtSF','Heating','HeatingQC','CentralAir','Electrical','1stFlrSF','2ndFlrSF','LowQualFinSF','GrLivArea','BsmtFullBath','BsmtHalfBath','FullBath','HalfBath','BedroomAbvGr','KitchenAbvGr','KitchenQual','TotRmsAbvGrd','Functional','Fireplaces','FireplaceQu','GarageType','GarageYrBlt','GarageFinish','GarageCars','GarageArea','GarageQual','GarageCond','PavedDrive','WoodDeckSF','OpenPorchSF','EnclosedPorch','3SsnPorch','ScreenPorch','PoolArea','PoolQC','Fence','MiscFeature','MiscVal','MoSold','YrSold','SaleType','SaleCondition']]
 Y = df.SalePrice
 
-# TODO: word2vec
-
-x_train = np.array(X)
-y_train = np.array(Y)
-
-clf = RandomForestClassifier(n_estimators=20, n_jobs=3)
-clf.fit(x_train, y_train)
-
-embed()
+# x_train = np.array(X)
+# y_train = np.array(Y)
+# 
+# clf = RandomForestClassifier(n_estimators=20, n_jobs=3)
+# clf.fit(x_train, y_train)
+# 
+# embed()
