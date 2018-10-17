409978546fa840c26a1f9df4ee62997c40a2730b
kenta-s <knt01222@gmail.com>
2018-10-17 09:03:53 +0900

    wip

--- house_prices/20181016/house_prices.py
+++ house_prices/20181016/house_prices.py
@@ -5,14 +5,27 @@ import sklearn.model_selection as sk
 from gensim import corpora
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
 
 from IPython import embed
 
 df = pd.read_csv("train.csv")
+processed_df = df
 nans = (df.isnull().sum() / len(df)) * 100
 for colum in list(df):
     if(nans[colum] != 0):
         print(colum)
         print(df[colum].isnull().sum())
 df.LotFrontage.fillna(0)
+
+transform_list = ["MSZoning"]
+for column in transform_list:
+    le = LabelEncoder()
+    le.fit(df[column])
+    processed_df[column] = le.transform(df[column])
+
+fillna_columns = ["LotFrontage"]
+for column in fillna_columns:
+    processed_df[column] = processed_df[column].fillna(0)
+
 embed()
