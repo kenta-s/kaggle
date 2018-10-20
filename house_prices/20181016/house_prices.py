aaa092b9a8b4f598310b048b55fe8ae725e680f2
kenta-s <knt01222@gmail.com>
2018-10-21 00:36:07 +0900

    Fix bug

--- house_prices/20181016/house_prices.py
+++ house_prices/20181016/house_prices.py
@@ -13,8 +13,73 @@ from IPython import embed
 
 df = pd.read_csv("train.csv")
 
-# corr()でstrがすべて除外されてしまうのでこのままじゃだめ
-to_be_used = df.corr()[df.corr().SalePrice > 0.2].index
+strColumns = [
+    'MSZoning',
+    'Street',
+    'Alley',
+    'LotShape',
+    'LandContour',
+    'Utilities',
+    'Fence',
+    'MiscFeature',
+    'SaleType',
+    'SaleCondition',
+    'PoolQC',
+    'BldgType',
+    'BsmtCond',
+    'BsmtExposure',
+    'BsmtFinType1',
+    'BsmtFinType2',
+    'BsmtQual',
+    'CentralAir',
+    'Condition1',
+    'Condition2',
+    'Electrical',
+    'ExterCond',
+    'ExterQual',
+    'Exterior1st',
+    'Exterior2nd',
+    'FireplaceQu',
+    'Foundation',
+    'Functional',
+    'GarageCond',
+    'GarageFinish',
+    'GarageQual',
+    'GarageType',
+    'Heating',
+    'HeatingQC',
+    'HouseStyle',
+    'KitchenQual',
+    'LandSlope',
+    'LotConfig',
+    'MasVnrType',
+    'Neighborhood',
+    'PavedDrive',
+    'RoofMatl',
+    'RoofStyle'
+]
+
+processed_df = df
+def transform_df(target_df):
+    for column in strColumns:
+        target_df[column] = target_df[column].fillna('None')
+        le = LabelEncoder()
+        le.fit(target_df[column].unique())
+        target_df[column] = le.transform(target_df[column])
+    return target_df
+
+def process_data(target_df):
+    for column in target_df.columns:
+        if str in map(lambda x: type(x), target_df[column]):
+            target_df[column] = target_df[column].fillna('None')
+        else:
+            target_df[column] = target_df[column].fillna(0)
+    return target_df
+
+processed_df = transform_df(processed_df)
+processed_df = process_data(processed_df)
+
+to_be_used = processed_df.corr()[processed_df.corr().SalePrice.abs() > 0.2].index
 
 # GarageYrBltはYearBltとの相関が高い 0.825667
 # 1stFlrSFはTotalBsmtSFとの相関が高い 0.819530
@@ -22,24 +87,18 @@ to_be_used = df.corr()[df.corr().SalePrice > 0.2].index
 to_be_used = to_be_used.drop(["GarageYrBlt", "1stFlrSF", "GarageArea"])
 processed_df = df[to_be_used]
 
-def process_data(target_df):
-    for column in target_df.columns:
-        target_df[column] = target_df[column].fillna(0)
-    return target_df
-
-processed_df = process_data(processed_df)
-
 X = processed_df.drop('SalePrice', axis=1)
 Y = processed_df.SalePrice
 x_train, x_test, y_train, y_test = sk.train_test_split(X, Y, test_size=0.1)
-clf = RandomForestRegressor(n_estimators=1300, max_depth=7, n_jobs=10)
+clf = RandomForestRegressor(n_estimators=1700, max_depth=12, n_jobs=10)
 clf.fit(x_train, y_train)
 
 rmse = np.sqrt(mean_squared_error(y_test, clf.predict(x_test)))
 print("RMSE: " + str(rmse))
 
 raw_data = pd.read_csv("../test.csv")
-test_data = pd.read_csv("../test.csv")[to_be_used.drop('SalePrice')]
+test_data = transform_df(raw_data)
+test_data = test_data[to_be_used.drop('SalePrice')]
 test_data = process_data(test_data)
 result = clf.predict(test_data)
 ans_csv = pd.concat((raw_data.Id, pd.DataFrame(result)), axis=1)
